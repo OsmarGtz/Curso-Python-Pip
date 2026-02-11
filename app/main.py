96897d977f092c72c9a187630c99fb413e61d8f3
OsmarGtz <osmar@OSMAR-LAPTOP.>
2026-02-11 11:18:25 -0500

    added web server

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 import mod
-import read_csv
 import charts
+import pandas as pd
 
 def run():
+    '''
     data = read_csv.read_csv('data.csv')
     countries = list(map(lambda x: x['Country/Territory'], data))
     porcentages = list(map(lambda x: float(x['World Population Percentage']), data))
-    charts.generate_pie_chart(countries, porcentages, 'World Population Percentage by Country')
+    '''
 
-    
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'South America']
+    countries = df['Country/Territory'].values
+    porcentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries, porcentages, 'Population Percentage by Country')
+
     country = input('Type country => ')
-    result = mod.get_population(mod.population_by_country(data, country))
+    result = mod.get_population(mod.population_by_country(df.to_dict('records'), country))
     print(result)
 
     if len(result) > 0:
-        years, population = mod.get_population(mod.population_by_country(data, country))
+        years, population = mod.get_population(mod.population_by_country(df.to_dict('records'), country))
         charts.generate_bar_chart(years, population, f'Population in {country} over Years')
     
 
